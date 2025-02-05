03b00ec3979dcf19e33767706316a0073c05dc9b
Your Name <yourname@example.com>
2025-02-05 10:29:13 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import { config as configDotenv } from "dotenv";
 import connectDB from "./Db/Db.js";
+import cors from "cors"
+import authRoutes from "./routes/authRoutes.js"
+import bodyParser from "body-parser";
+
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Load environment variables
 configDotenv();
 connectDB();
 
-app.use("/", (req, res) => {
-    res.send("Hello world");
-});
+app.use('/api/auth', authRoutes);
 
-app.listen(4000, () => {
-    console.log("Server running at http://localhost:4000");
+app.listen(5000, () => {
+    console.log("Server running at http://localhost:5000");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Auth/Sign";
+import Login from "./Auth/Login";
+import Home from "./Pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
