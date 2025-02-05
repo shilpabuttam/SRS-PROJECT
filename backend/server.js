1848ca15770590ec998b9b01ffeb2107e8a1c3bf
Pavan <pavan.ediga@meenews.co>
2025-02-05 14:04:20 +0530

    Contact component

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,22 @@ import connectDB from "./Db/Db.js";
 import cors from "cors"
 import authRoutes from "./routes/authRoutes.js"
 import bodyParser from "body-parser";
+import {contactRouting} from "./routes/contactRouting.js";
 
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}))
 
 // Load environment variables
 configDotenv();
 connectDB();
 
 app.use('/api/auth', authRoutes);
+app.use('/api', contactRouting);
 
-app.listen(5000, () => {
-    console.log("Server running at http://localhost:5000");
+app.listen(4000, () => {
+    console.log("Server running at http://localhost:4000");
 });
